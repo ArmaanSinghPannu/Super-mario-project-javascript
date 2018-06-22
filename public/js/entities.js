e11d39e2b11082e95cd2fee7ce7c388012173a9f
Armaan Singh Pannu <armaanpannu1205@gmail.com>
2018-06-22 11:28:22 -0700

    first commit

---
+++ public/js/entities.js
@@ -0,0 +1,33 @@
+    import Entity from './entity.js';
+    import Jump from './traits/Jump.js';
+    import Go from './traits/Go.js';
+    import {loadSpriteSheet} from './loaders.js';
+
+    export function createMario() {
+        return loadSpriteSheet('mario')
+        .then(sprite =>{
+            const mario = new Entity();
+            mario.size.set(14, 16);
+                
+            mario.addTrait(new Go());
+            mario.addTrait(new Jump());
+
+            const frames = ['run-1', 'run-2', 'run-3'];
+
+            function routeFrame(mario) {
+                if (mario.go.dir !== 0) {
+                    const frameIndex = Math.floor(mario.go.distance / 10) % frames.length;
+                   const frameName = frames[frameIndex];
+                    return frameName;
+                }
+
+                return 'idle';
+            }
+            
+        mario.draw = function drawMario(context) {
+            sprite.draw(routeFrame(this), context, 0, 0);
+        } 
+        
+        return mario;
+    });
+}
